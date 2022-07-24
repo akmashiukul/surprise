2a68cb0b1cf86baae7727291e76308ec8a1e5b52
akmashiukul <akmashikul@gmail.com>
2022-07-24 11:23:32 +0600

    add

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Hello");
+function no() {
+  document.getElementById("body").innerHTML = "Thank you for everything 😞.";
+}
+function yes() {
+  document.getElementById("body").innerHTML =
+    "<h2 style=' color: deeppink;margin-top: 99px;margin-left: 46px;font-size: 30px;' >I love you princess 😘</h2>";
+}
