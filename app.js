894cd33393be008aa2808890fc0eff53b1229eb0
akmashiukul <akmashikul@gmail.com>
2022-07-24 14:07:35 +0600

    add

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 console.log("Hello");
 function no() {
-  document.getElementById("body").innerHTML = "Thank you for everything 😞.";
+  document.getElementById("body").innerHTML =
+    "Thank you for everything 😞.তোমাকে আর জ্বালাবো না। ধন্যবাদ";
 }
 function yes() {
   document.getElementById("body").innerHTML =
-    "<h2 style=' color: deeppink;margin-top: 99px;margin-left: 46px;font-size: 30px;' >I love you princess 😘</h2>";
+    "<h2 style=' color: deeppink;margin-top: 99px;margin-left: 46px;font-size: 30px;' >I love you princess 😘। তোমাকে এভাবে জ্বালাতে থাকবো সহ্য করতে পারবে?</h2>";
 }
